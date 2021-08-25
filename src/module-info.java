a2542e93261f7498871d8635664122665b634c1a
Muzashi <evertoncosta100@gmail.com>
2021-08-25 10:27:03 -0300

    Adicionando acesso ao banco de dados

--- src/module-info.java
+++ src/module-info.java
@@ -2,6 +2,7 @@ module JavaFX {
 	requires javafx.controls;
 	requires javafx.fxml;
 	requires javafx.graphics;
+	requires java.sql;
 	opens model.entities to javafx.graphics, javafx.fxml, javafx.base;
 	
 	exports gui;
